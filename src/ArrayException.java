9da891d545102a6f0e9a2093105db4ce2c081ad4
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2026-01-26 09:56:38 +0100

    Initial commit Övning 1

---
+++ src/ArrayException.java
@@ -0,0 +1,12 @@
+package java;
+// Följande array finns i ditt program:
+// String[] names = {"Ada", "Beda", "Cålle"};
+//
+//1. Läs in ett tal som beskriver vilket namn
+// man skall skriva ut i ordningen. Fånga eventuella exceptions
+public class ArrayException {
+    public static void main(String[] args) {
+        String[] names = {"Ada", "Beda", "Cålle"};
+        System.out.println(names);
+    }
+}
