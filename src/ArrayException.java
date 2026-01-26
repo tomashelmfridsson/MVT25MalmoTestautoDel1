d919660a7200c1c93304fb53c0ef54dbf54c2606
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2026-01-26 09:57:37 +0100

    Med try and catch

--- src/ArrayException.java
+++ src/ArrayException.java
@@ -1,12 +1,19 @@
-package java;
-// Följande array finns i ditt program:
-// String[] names = {"Ada", "Beda", "Cålle"};
-//
+import java.util.Arrays;
+import java.util.Scanner;
 //1. Läs in ett tal som beskriver vilket namn
 // man skall skriva ut i ordningen. Fånga eventuella exceptions
+// 2. Uppdatera ditt program så att det fångar exceptions
+// vid olika tillfällen (inläsning av tal respektive utskrift av namn)
 public class ArrayException {
     public static void main(String[] args) {
-        String[] names = {"Ada", "Beda", "Cålle"};
-        System.out.println(names);
+        try {
+            String[] names = {"Ada", "Beda", "Cålle"};
+            System.out.println(Arrays.toString(names));
+            Scanner myScan = new Scanner(System.in);
+            int index = myScan.nextInt();
+            System.out.println(names[index]);
+        } catch(Exception exception){
+            System.out.println(exception.getMessage());
+        }
     }
 }
